1cb58618449bc28c02f8d7201695e3edb07d9231
DashaGKH <keshonti2016@gmail.com>
2024-07-29 21:25:10 -0700

    fix

--- models/Comment.js
+++ models/Comment.js
@@ -1,34 +1,25 @@
-const mongoose = require('mongoose');
-const {model, Schema} = require('mongoose');
-mongoose.Schema.Types.String.set('trim', true)
+const { model, Schema} = require('mongoose')
+const mongoose = require("mongoose");
+mongoose.Schema.Types.String.set('trim', true);
 
-const CommentSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    rating: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 5
-    },
-    title: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    }
-})
+const commentSchema =
 
-module.exports = model("Comment", CommentSchema);
+    new Schema({
+        _id: mongoose.Schema.Types.ObjectId,
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+
+        createdAt: String,
+
+        rating: Number,
+
+        title: String,
+
+        description: String,
+
+    });
+
+module.exports = model('Comment', commentSchema)
--- models/User.js
+++ models/User.js
@@ -5,12 +5,10 @@ mongoose.Schema.Types.String.set('trim', true);
 const userSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     firstName: {
-        type: String,
-        trim: true
+        type: String
     },
     lastName: {
-        type: String,
-        trim: true
+        type: String
     },
     comments: [
         {
